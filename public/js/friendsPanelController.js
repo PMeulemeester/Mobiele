e713859953b8c0e066b5a7e2364da321e6a3b09f
landotanghe <lando.tanghe@gmail.com>
2015-04-29 13:26:54 +0200

    friends front end
    verwijderen en toevoegen nog inefficiënt: vereist volledig opnieuw
    ophalen van de lijsten vrienden, requestsSent en requestsReceived

--- public/js/friendsPanelController.js
+++ public/js/friendsPanelController.js
@@ -1,23 +1,55 @@
 /**
  * Created by pieterm on 23/04/15.
  */
+
+
 app.controller('friendsPanelController',['$scope','$window','$http',function($scope,$window,$http){
     $scope.naam = "Mobiele app - Friends";
     //console.log('test');
-    $http.get('../public/friends').success(function(response){
-        $scope.friends=response;
-        console.log(response);
-    });
+
+    $scope.loadFriends = function(){
+        $http.get('../public/friends').success(function(response){
+            $scope.friends=response;
+            console.log(response);
+        });
+    }
+
+    $scope.loadReceived = function(){
+        $http.get('../public/friends/received').success(function(response){
+            $scope.received=response;
+            console.log(response);
+        });
+    }
 
 
-    $http.get('../public/friends/sent').success(function(response){
-        $scope.sent=response;
-        console.log(response);
-    });
+    $scope.loadSent = function(){
+        $http.get('../public/friends/sent').success(function(response){
+            $scope.sent=response;
+            console.log(response);
+        });
+    }
 
+    $scope.loadFriends();
+    $scope.loadReceived();
+    $scope.loadSent();
 
-    $http.get('../public/friends/received').success(function(response){
-        $scope.received=response;
-        console.log(response);
-    });
+    $scope.addFriend=function(friend){
+      //  window.alert("add " + friend.id);
+        var id = friend.id;
+        $http.get('../public/friends/add/' + id).success(function(response){
+            $scope.loadFriends();
+            $scope.loadReceived();
+            $scope.loadSent();
+        });
+    }
+    $scope.removeFriend=function(friend){
+      //  window.alert("delete " + friend.id);
+        var id = friend.id;
+        $http.get('../public/friends/del/' + id).success(function(response){
+            $scope.loadFriends();
+            $scope.loadReceived();
+            $scope.loadSent();
+
+        });
+    }
 }]);
--- public/views/friends.blade.php
+++ public/views/friends.blade.php
@@ -1,28 +1,48 @@
 <div>
     <p>{{naam}}</p>
     <p>hallo</p>
-    <table>
+    <table class="table">
         <tr>
             <th>Friends</th>
         </tr>
         <tr ng-repeat="friend in friends">
-            <td>{{friend.email}}</td>
+            <td>
+                {{friend.email}}
+            </td>
+            <td>
+                <button class="btn btn-danger" type="button" ng-click="removeFriend(friend)">
+                    <span class="glyphicon glyphicon-remove"></span>
+                </button>
+            </td>
         </tr>
-    </table>
-    <table>
         <tr>
             <th>Accept requests</th>
         </tr>
         <tr ng-repeat="request in received">
-            <td>{{request.email}}</td>
+            <td>
+                {{request.email}}
+            </td>
+            <td>
+                <button class="btn btn-success" type="button" ng-click="addFriend(request)">
+                    <span class="glyphicon glyphicon-ok"></span>
+                </button>
+                <button class="btn btn-danger" type="button" ng-click="removeFriend(request)">
+                    <span class="glyphicon glyphicon-remove"></span>
+                </button>
+            </td>
         </tr>
-    </table>
-    <table>
         <tr>
             <th>Waiting for acceptance</th>
         </tr>
         <tr ng-repeat="request in sent">
-            <td>{{request.email}}</td>
+            <td>
+                {{request.email}}
+            </td>
+            <td>
+                <button class="btn btn-success" type="button" ng-click="addFriend(request)">
+                    <span class="glyphicon glyphicon-ok"></span>
+                </button>
+            </td>
         </tr>
     </table>
 </div>
